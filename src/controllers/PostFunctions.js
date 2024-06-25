9daa6db9ca9f2d2645a9e0e353e7bdf4c97533de
Willem Gibson <129262358+WillemGibson@users.noreply.github.com>
2024-06-25 18:01:37 +1000

    feat: creating post routes and adding server functionality; also adding some commenting

--- src/controllers/PostFunctions.js
+++ src/controllers/PostFunctions.js
@@ -1,31 +1,36 @@
 const { Post } = require('../models/PostModel');
 
-// Model.find({}) returns all documents in a collection.
+// Model.find({}) returns all documents in the 'posts' collection.
 async function getAllPosts() {
+    // Retrieve all posts from the database.
     return await Post.find({}).exec();
 }
 
 async function getPostById(postID) {
+    // Retrieve a post by its unique ID.
     return await Post.findById(postID).exec();
 }
 
 async function getPostByAuthor(userID) {
-    return await Post.find({author: userID}).exec();
+    // Retrieve all posts authored by a specific user.
+    return await Post.find({ author: userID }).exec();
 }
 
 async function createPost(postDetails) {
+    // Create a new post using the provided postDetails.
     return await Post.create(postDetails);
 }
 
 async function updatePost(postDetails) {
-    // Find user, update it,return the updated user data.
-    return await Post.findByIdAndUpdate(postDetails.postID, postDetails.updatedData, {returnDocument: 'after'}).exec();
+    // Find a post by ID, update it with new data, and return the updated post.
+    return await Post.findByIdAndUpdate(postDetails.postID, postDetails.updatedData, { returnDocument: 'after' }).exec();
 }
 
 async function deletePost(postID) {
+    // Find a post by ID and delete it from the database.
     return await Post.findByIdAndDelete(postID).exec();
 }
 
 module.exports = {
-    getAllPosts, getPostById, getPostsByAuthor, createPost, updatePost, deletePost
+    getAllPosts, getPostById, getPostByAuthor, createPost, updatePost, deletePost
 }
